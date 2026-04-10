5af005107c7f0270c0bdd12ee068c581a63eaea7
Giovani <giovani98@proton.me>
2026-04-10 20:52:22 -0300

    feat: WiP - Separation of concerns

--- src/app.ts
+++ src/app.ts
@@ -1,18 +1,6 @@
 import fastify from 'fastify'
-import { PrismaPg } from '@prisma/adapter-pg'
-import { PrismaClient } from '../generated/prisma/client.js'
-import { env } from './env.js'
+import { appRoutes } from './http/routes.js'
 
 export const app = fastify()
 
-// Database Setup
-const connectionString = env.DATABASE_URL
-const adapter = new PrismaPg({ connectionString })
-const prisma = new PrismaClient({ adapter })
-
-prisma.user.create({
-  data: {
-    email: 'teste@teste.com',
-    name: 'John Doe',
-  },
-})
+app.register(appRoutes)
