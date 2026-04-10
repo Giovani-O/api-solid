077f6048de60b774c090c707e29125b9b66f38ef
Giovani <giovani98@proton.me>
2026-04-10 16:48:36 -0300

    Add Prisma ORM with PostgreSQL setup

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,18 @@
 import fastify from 'fastify'
+import { PrismaPg } from '@prisma/adapter-pg'
+import { PrismaClient } from '../generated/prisma/client.js'
+import { env } from './env.js'
 
 export const app = fastify()
+
+// Database Setup
+const connectionString = env.DATABASE_URL
+const adapter = new PrismaPg({ connectionString })
+const prisma = new PrismaClient({ adapter })
+
+prisma.user.create({
+  data: {
+    email: 'teste@teste.com',
+    name: 'John Doe',
+  },
+})
