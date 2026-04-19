93b237b01e0a958faa9a74ba791d3700715fdfc5
Giovani <giovani98@proton.me>
2026-04-19 15:16:32 -0300

    feat: WiP - Add JWT

--- src/http/routes.ts
+++ src/http/routes.ts
@@ -1,8 +1,12 @@
 import type { FastifyInstance } from 'fastify'
 import { authenticate } from './controllers/authenticate.controller.js'
+import { profile } from './controllers/profile.controller.js'
 import { register } from './controllers/register.controller.js'
 
 export async function appRoutes(app: FastifyInstance) {
   app.post('/users', register)
   app.post('/sessions', authenticate)
+
+  /** Authenticated  */
+  app.get('/me', profile)
 }
