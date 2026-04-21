506a28414d80b5e5a9073a083d112bc04264e3ee
Giovani <giovani98@proton.me>
2026-04-21 18:26:42 -0300

    feat: register controller e2e test

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -3,7 +3,10 @@ import { env } from '@/env.js'
 import { PrismaClient } from '../../generated/prisma/client.js'
 
 const connectionString = env.DATABASE_URL
-const adapter = new PrismaPg({ connectionString })
+export const schema =
+  new URL(env.DATABASE_URL).searchParams.get('schema') || 'public'
+
+const adapter = new PrismaPg({ connectionString }, { schema })
 export const prisma = new PrismaClient({
   adapter,
   log: env.NODE_ENV === 'dev' ? ['query'] : [],
